7ed1f1901aa23eb144060a24a75c6434c456b49f
ZacheryGlass <ZachGlass96@gmail.com>
2016-08-17 17:09:23 -0500

    update

--- opengl/OpenGL_Matrices_Demo.cpp
+++ opengl/OpenGL_Matrices_Demo.cpp
@@ -1,7 +1,7 @@
 // File name:		OpenGL_Matrices_Demo.cpp
 // Author:			Zachery Glass
 // Date:			May 15, 2016
-// Despription:		This program draw multiple shapes and demonstrates the uses of a rotation, translation, sheer and scale matrices. 
+// Description:		This program draw multiple shapes and demonstrates the uses of a rotation, translation, sheer and scale matrices. 
 //					The user must press the UP and DOWN arrow keys to change movement speed of the shapes.
 
 #include <iostream>
